42b9ba517e40d8a37949cc26342bf3d5594ea7e6
koyotex <david242455@gmail.com>
2023-07-07 03:03:42 -0400

    mejorando css responsive adminpanel

--- js/adminPanel.js
+++ js/adminPanel.js
@@ -1,13 +1,16 @@
 const uil = document.querySelector(".sidebar-toggle")
 const bodyAdminPanel = document.querySelector(".body-adminPanel")
+const dashboard = document.querySelector(".dashboard")
 uil.addEventListener("click", () => {
     // alert("asdas")
     if (window.innerWidth > 800) {
 
+        dashboard.classList.toggle("dashboard-reduce")
         bodyAdminPanel.classList.toggle("nav-adminPanelHidden")
     } else {
         // alert("asdas")
         //bodyAdminPanel.classList.remove("nav-adminPanelHidden")
+        dashboard.classList.toggle("dashboard-reduce-mobile")
         bodyAdminPanel.classList.toggle("nav-adminPanelHiddenMobil")
     }
 })
